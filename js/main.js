91077e5853a1176d45924a3b278ca9a52f56ac11
mpw10d <mpw10d@gmail.com>
2019-12-13 19:54:29 -0500

    FinalLab
    For Programming Course

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//function to start the Leaflet map
+function createMap() {
+console.log('shiiiiiit')
+    //Varibles for attribution box on the map.
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' +'  <a href="http://mapbox.com">Mapbox</a>'; 
+    //You can add you name to the variable if you want to add yourself to the credits.
+    
+    //Variable for storing your Mapbox API Token
+    var apitoken = 'pk.eyJ1IjoibXB3MTBkIiwiYSI6ImNrNDNteDlyaTA2cTIzbm12cWhyMXZmdzMifQ.fsIPIH1nF9RJ1CiNPUV-2A' 
+    //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+    
+     //URL used for Stanard MaxBox Styles
+    var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
+    
+    //URL used for Custom MapBox Style
+    var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}';
+    
+    //For Custome basemaps - Replace your username and StyleID
+    var customeBasemap = L.tileLayer(mbStyleUrl, {id: 'mpw10d/ck43mzgc80aol1cltskig7pxy', token: apitoken,  attribution: mbAttr});
+    
+    //For MabBox Standard Basemaps
+    var standardBasemap   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    
+    //create the map*/
+    var map = L.map('map', {
+        center: [31.00, -5],//Coordinated to center the map
+        zoom: 6, //zoom level
+        layers: customeBasemap //basemap
+    } )
+    
+};
+
+//calling the function
+$(document).ready(createMap);
